cdbb4f6a9df51578ebf31af7bd48bbe4040bd704
ndwalep <ndwalepatience@gmail.com>
2023-04-20 12:50:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
-        printf("\"with proper grammar, but the outcome is a piece of art");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
