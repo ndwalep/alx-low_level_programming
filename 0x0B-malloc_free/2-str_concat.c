63f82d60e78550d0a26fa32d70ce00b3b247cf37
ndwalep <ndwalepatience@gmail.com>
2023-05-15 13:57:08 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,33 +9,33 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int p, kc;
+	int p, ci;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	p = kc = 0;
+	p = ci = 0;
 	while (s1[p] != '\0')
 		p++;
-	while (s2[kc] != '\0')
-		kc++;
-	conct = malloc(sizeof(char) * (p + ki + 1));
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (p + ci + 1));
 
 	if (conct == NULL)
 		return (NULL);
-	p = ki = 0;
+	p = ci = 0;
 	while (s1[p] != '\0')
 	{
 		conct[p] = s1[p];
 		p++;
 	}
 
-	while (s2[kc] != '\0')
+	while (s2[ci] != '\0')
 	{
-		conct[p] = s2[kc];
-		p++, kc++;
+		conct[p] = s2[ci];
+		p++, ci++;
 	}
 	conct[p] = '\0';
 	return (conct);
